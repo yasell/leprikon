855e1181e70b78dc4450f5baac9d7fbe916af260
yasell <raman.yasel@yahoo.com>
2016-11-08 17:25:29 +0300

    1st part

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function ($) {
+	$body = $('body');
+	windowWidth = $(window).width();
+	windowHeight = $(window).height();
+
+	//developer funcitons
+	getAllClasses('html','.elements_list');
+});
